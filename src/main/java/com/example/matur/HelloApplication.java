db5fb46548c5eb008818d55462b7a84c62b91edd
luke <lukashelesi56@.com>
2024-04-04 09:59:00 +0200

    Clear codebase

--- src/main/java/com/example/matur/HelloApplication.java
+++ src/main/java/com/example/matur/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.matur;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,7 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));//dawdwa
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("login");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
